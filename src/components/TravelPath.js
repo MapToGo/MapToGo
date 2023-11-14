d72793a2a15bd2d4aaff4958c9af07c1080ea0bc
Putipong <16.champ.16@gmail.com>
2023-11-14 20:13:32 +0700

    may be final

--- src/components/TravelPath.js
+++ src/components/TravelPath.js
@@ -1,23 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import worldIcon from "../image/world.svg";
 import locationIcon from "../image/location.svg";
 import tripleDot from "../image/tripleDot.svg";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, query, where, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 
-const Travelpath = () => {
+const TravelPath = () => {
+  const [userData, setUserData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const auth = getAuth();
+      const userEmail = auth.currentUser.email;
+      const db = getFirestore();
+      const q = query(collection(db, 'FavP'), where('email', '==', userEmail));
+      const querySnapshot = await getDocs(q);
+
+      const data = [];
+      querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+
+      setUserData(data);
+    } catch (error) {
+      console.error("Error getting documents: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      const db = getFirestore();
+      await deleteDoc(doc(db, 'FavP', id));
+      // Fetch updated data after deletion
+      fetchData();
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
+  };
+  let navigate = useNavigate();
+  const handleGoToMap = (user) => {
+   
+    console.log("Navigate to map with start:", user.start, "and end:", user.end);
+    navigate("/map");
+  };
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row space-x-4">
-        <img src={worldIcon} alt="worldIcon" hight="25" width="25" />
-        <p className="border rounded-xl p-5 px-28 drop-shadow-md bg-white "/>
-      </div>
-      <img src={tripleDot} alt="tripledot" hight= "25" width="25"/>      
-      <div className="flex flex-row space-x-4">
-        <img src={locationIcon} alt="worldIcon" hight="25" width="25" />
-        <p className="border rounded-xl p-5 px-28 drop-shadow-md bg-white "/>
-      </div>
+      {userData.map((user, index) => (
+        <div key={index}>
+          <div className="flex flex-row space-x-4">
+            <img src={worldIcon} alt="worldIcon" height="25" width="25" />
+            <p className="border rounded-xl p-5 px-28 drop-shadow-md bg-white">
+              {user.start}
+            </p>
+          </div>
+          <img src={tripleDot} alt="tripledot" height="25" width="25" />
+          <div className="flex flex-row space-x-4">
+            <img src={locationIcon} alt="worldIcon" height="25" width="25" />
+            <p className="border rounded-xl p-5 px-28 drop-shadow-md bg-white">
+              {user.end}
+            </p>
+          </div>
+          <br/>
+          <button onClick={() => handleDelete(user.id)}>Delete</button>
+          <br/>
+          <button 
+          onClick={() => handleGoToMap(user)}
+          
+          >Go to Map</button>
+          <br/>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Travelpath;
+export default TravelPath;
